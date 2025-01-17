c3d6b4f1fb7fa4569190b13abae97283942a0a3f
i-eat-rocks <143463845+i-eat-rocks@users.noreply.github.com>
2025-01-16 18:50:36 -0600

    day 2 changes #3

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
+const PORT = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Hello Cloud!')
 })
 
-app.listen(3000)
+app.listen(PORT, ()=> console.log(`server is running on ... ${PORT}`))
