a3efdc1550fb34fe4147f40e06bcdd42881148a2
i-eat-rocks <143463845+i-eat-rocks@users.noreply.github.com>
2025-01-23 00:42:45 -0600

    start #7

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+require('dotenv').config();
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + "/public"));
 // console.log(__dirname);
 
+// Endpoints //
 app.get('/', function (req, res) {
   res.sendFile("index.html");
 
